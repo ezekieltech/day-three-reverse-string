c6287883d986737742579e1572cff16d40da7fb7
ezekieltech <ezekielobhafuoso@gmail.com>
2017-05-11 15:03:53 +0100

    Corrected for failed case

--- app/reverseString.js
+++ app/reverseString.js
@@ -1,12 +1,17 @@
 'use strict'
 
 function reverseString(string){
+  if (string.length === 0) { 
+    return null
+    
+  }else{
   var array = string.split('');
   var newstring = [];
   for (var i = string.length-1; i>=0; --i){
     newstring += array[i];
   }
   return newstring===string ? true: newstring;
+  }
   
 }
 
